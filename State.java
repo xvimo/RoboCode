cf54b8f07664f031bdb05346f9b21508ab4c1dd3
Cristiantor83 <160825107+Cristiantor83@users.noreply.github.com>
2024-10-18 10:05:17 +0200

    Update State.java

--- State.java
+++ State.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package robottimid;
+package timidinrobot;
+import robocode.ScannedRobotEvent;
 
-/**
- *
- * @author cris
- */
+
 public interface State {
 
-    public void doAction();
+    public void doAction(robotContext context);
+    void onScannedRobot(robotContext context, ScannedRobotEvent e);
 }
