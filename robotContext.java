257fd050a06d77d8db8de5888bddba84bb308ccb
Cristiantor83 <160825107+Cristiantor83@users.noreply.github.com>
2024-10-18 10:06:43 +0200

    Update robotContext.java

--- robotContext.java
+++ robotContext.java
@@ -2,23 +2,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package robottimid;
+package timidinrobot;
+
+import robocode.ScannedRobotEvent;
 
 /**
  *
  * @author cris
  */
-public class robotContext implements State{
-    private State robotState;
-    public void setState(State state){
-        this.robotState = state;
+public class robotContext{
+   private State currentState;
+   private TimidinRobot robot;
+   
+   public robotContext(TimidinRobot robot){
+       this.robot = robot;
+       this.currentState = new detectaEnemic();
+   }
+   
+   public void setState(State state){
+       this.currentState = state;
+   }
+   
+   public void execute(){
+       currentState.doAction(this);
+   }
+   
+    public void onScannedRobot(ScannedRobotEvent e) {
+        currentState.onScannedRobot(this, e);  // Delegate to the current state's scanning logic
     }
-    public State getState(){
-        return this.robotState;
+
+    public TimidinRobot getRobot() {
+        return robot;
     }
-    @Override
-    public void doAction() {
-        this.robotState.doAction();
-    }
-    
 }
