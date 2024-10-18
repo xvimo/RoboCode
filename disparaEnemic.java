21cc0743aa91d2cad8727f02c2121ed298c3be9f
Cristiantor83 <160825107+Cristiantor83@users.noreply.github.com>
2024-10-18 20:19:01 +0200

    Update disparaEnemic.java

--- disparaEnemic.java
+++ disparaEnemic.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package robottimid;
+package timidinrobot;
+
+import robocode.ScannedRobotEvent;
 
 /**
  *
@@ -11,7 +13,14 @@ package robottimid;
 public class disparaEnemic implements State{
 
     @Override
-    public void doAction() {
+    public void doAction(robotContext context) {
+        //TimidinRobot robot = context.getRobot();
+        
+        
+    }
+
+    @Override
+    public void onScannedRobot(robotContext context, ScannedRobotEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
